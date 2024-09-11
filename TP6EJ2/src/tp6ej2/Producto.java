40d50bb83c119cbe82f432330b219d31afddb54c
KJC-dev11 <kevinjofre1111@gmail.com>
2024-09-11 17:50:41 -0300

    Class producto

---
+++ TP6EJ2/src/tp6ej2/Producto.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp6ej2;
+
+/**
+ *
+ * @author MOON
+ */
+public class Producto {
+    private int codigo;
+    private int stock;
+    private double precio;
+    private String rubro;
+    private String descripcion;
+
+    public Producto(int codigo, int stock, double precio, String rubro, String descripcion) {
+        this.codigo = codigo;
+        this.stock = stock;
+        this.precio = precio;
+        this.rubro = rubro;
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
